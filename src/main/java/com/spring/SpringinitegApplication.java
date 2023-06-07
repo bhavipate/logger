4da341ef32605fc24a52d0419989a21456c2f59e
bhavipate <bhavipatel2323@gmail.com>
2023-06-07 09:25:06 +0530

    first commit

---
+++ src/main/java/com/spring/SpringinitegApplication.java
@@ -0,0 +1,22 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+import org.slf4j.*;
+@SpringBootApplication 
+public class SpringinitegApplication implements CommandLineRunner{
+	public static final Logger Logger= LoggerFactory.getLogger(SpringinitegApplication.class);
+	public static void main(String[] args)
+	{ 
+		Logger.info("its a CI pipeline job"); 
+		SpringApplication.run(SpringinitegApplication.class, args); 
+	} 
+	@Override 
+	public void run(String... args) throws Exception 
+	{
+		Logger.info("second log"); 
+    } 
+}
